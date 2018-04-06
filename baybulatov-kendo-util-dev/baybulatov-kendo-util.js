35eb9d390407051edf5cef9c7ec31ad8f3cf529b
Baybulatov.Artur <Baybulatov.Artur@wildberries.ru>
2018-04-06 20:47:51 +0300

    .

--- baybulatov-kendo-util-dev/baybulatov-kendo-util.js
+++ baybulatov-kendo-util-dev/baybulatov-kendo-util.js
@@ -4,61 +4,76 @@
     var kendoUtil = window.kendoUtil = {};
     kendoUtil._version = 'dev';
 
-
     var ensure = util.ensure;
 
 
-    var popup = kendoUtil.popup = function(title, $popup, callbacks, extraOptions) {
+    var popup = kendoUtil.popup = function(title, $popup, callbacks) {
         ensure.nonEmptyString(title);
         ensure.jqElement($popup);
-        ensure.maybe.plainObject(callbacks, extraOptions);
+        ensure.maybe.plainObject(callbacks);
 
         if (!_.isPlainObject(callbacks))
-            callbacks = {};
-
-        ensure.maybe.function(callbacks.open, callbacks.ok, callbacks.close);
+            callbacks = {}; // To prevent reference errors
 
         var options = {
-            visible: true,
+            visible: false,
             title: title,
             content: $popup,
             minWidth: 400,
             maxWidth: 800,
             maxHeight: 600,
+            actions: [],
         };
 
+        var kDialog = $('<div>').kendoDialog(options).data('kendoDialog');
+        var destroy = function() { kDialog.destroy(); $('.k-overlay').remove() };
+
+        if (typeof callbacks.ok === 'function') {
+            options.actions.push({
+                text: 'OK',
+                primary: true,
+                action: function() { callbacks.ok.call(kDialog, destroy); return false },
+            });
+        }
+
+        options.actions.push({ text: 'Close' });
+
         if (typeof callbacks.open === 'function')
-            options.open = callbacks.open;
+            options.open = callbacks.open.bind(kDialog, destroy);
 
         options.close = function() {
             if (typeof callbacks.close === 'function')
-                callbacks.close();
+                callbacks.close.call(kDialog);
 
             this.destroy();
         };
 
-        options.actions = [
-            {
-                text: 'OK',
-                primary: true,
-
-                action: function(evt) {
-                    var kDialog = evt.sender;
-
-                    if (typeof callbacks.ok === 'function') {
-                        callbacks.ok.call(kDialog);
-                        return false;
-                    } else {
-                        g.kDialog = kDialog;
-                        //kDialog.destroy();
-                        return false; // Tmp
-                    }
-                },
-            },
-
-            { text: 'Close' },
-        ];
-
-        return $('<div>').kendoDialog(Object.assign(options, extraOptions)).data('kendoDialog');
+        kDialog.setOptions(options);
+        kDialog.open();
     };
+
+
+    var $spinner = $('<div>', { html: $('<div>', { class: 'spinner', attr: { 'js-spinner': '' } }) });
+
+    var kSpinner = $spinner.kendoDialog({
+        visible: false,
+        closable: false,
+        title: '',
+        minWidth: 70,
+        minHeight: 70,
+        actions: [],
+    }).data('kendoDialog');
+
+    kSpinner.wrapper.find('.k-dialog-titlebar').remove();
+
+    kSpinner.wrapper.find(':not([js-spinner])').addBack().css({
+        margin: 0,
+        padding: 0,
+        backgroundColor: 'transparent',
+        border: 'none',
+        overflow: 'visible',
+    });
+
+    var blockUi = kendoUtil.blockUi = function() { kSpinner.open() };
+    var unblockUi = kendoUtil.unblockUi = function() { kSpinner.close() };
 }());
