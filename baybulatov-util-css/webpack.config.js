b40e5570da215287af7f464461963d769c837818
Baybulatov.Artur <Baybulatov.Artur@wildberries.ru>
2018-09-24 15:56:56 +0300

    Rename repo to baybulatov-util; Add baybulatov-util-css

---
+++ baybulatov-util-css/webpack.config.js
@@ -0,0 +1,34 @@
+﻿const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+const extractStyles = new ExtractTextPlugin('[name].css');
+
+module.exports = {
+    context: path.resolve('src/'), // Needs to be an absolute path
+    entry: { index: './index.less' },
+
+    output: {
+        path: path.resolve('dist/'), // Needs to be an absolute path
+        filename: '__ignore__.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+
+                use: extractStyles.extract({
+                    use: [
+                        { loader: 'raw-loader' }, // Instead of `css-loader` to prevent resolving `url(...)` paths
+                        { loader: 'postcss-loader', options: { plugins: [autoprefixer({ browsers: ['last 8 versions'] })] } },
+                        { loader: 'less-loader' },
+                    ],
+                }),
+            },
+        ],
+    },
+
+    plugins: [ extractStyles ],
+};
